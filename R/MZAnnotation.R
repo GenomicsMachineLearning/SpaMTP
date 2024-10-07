f469535ce470cf5c3ec8e0e0b3d73f10acbd3d88
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-10-07 14:48:11 +1100

    Update MZAnnotation -correct the error when doing AnnotateMZ
    There is an error when AnnotateMZ on SpaMTP object with more than 1 features

--- R/MZAnnotation.R
+++ R/MZAnnotation.R
@@ -186,7 +186,15 @@ AnnotateSM <- function(data, db, assay = "Spatial", raw.mz.column = "raw_mz", pp
 
     verbose_message(message_text = "Returning Seurat object that include ONLY SUCCESSFULLY ANNOTATED m/z features", verbose = verbose)
 
-    data <- SubsetMZFeatures(data, features = result_df$mz_names)
+    if(length(Assays(object = data)) != 1){
+      features = c()
+      for(non_met_assay in Assays(object = data)[which(Assays(object = data)!=assay)]){
+        features =  c(features, rownames(SpaMTP@assays[[non_met_assay]]@features))
+      }
+      data <- suppressWarnings({SubsetMZFeatures(data, assay = assay, features = c( result_df$mz_names, features))})
+    }else{
+      data <- suppressWarnings({SubsetMZFeatures(data, assay = assay, features = result_df$mz_names)})
+    }
   }
 
   return(data)
