038f8f317535bc82e8fda0e816e33933f8ac7f9a
agc888 <agcauser1997@gmail.com>
2025-09-17 21:59:53 +0200

    update SpaMTP V1.1 Vignettes

--- R/CardinalWrappers.R
+++ R/CardinalWrappers.R
@@ -8,15 +8,15 @@
 #'
 #' @param data A Cardinal Object containing the raw/binned m/z count data.
 #' @param data_ssc A Cardinal Object containing the ssc segmentation results. Note: Cardinal's spatialShrunkenCentroids() must be run to generate this object.
-#' @param resolution An integer defining the ssc segmentation resolution to add to the raw Cardinal object (default = 25).
+#' @param resolution An integer (< v3.6.0) or string (>= v3.6.0) defining the ssc segmentation resolution to add to the raw Cardinal object.
 #'
 #' @returns A Cardinal Object containing the raw m/z counts and the relative ssc segmentation annotations per pixel.
 #' @export
 #'
 #' @examples
 #' # ssc_data <- Cardinal::spatialShrunkenCentroids(CardinalObj, ...)
-#' # new_CardinalObj <- add_ssc_annotation(CardinalObj, ssc_data, resolution =25)
-add_ssc_annotation <- function(data, data_ssc, resolution = 25){
+#' # new_CardinalObj <- add_ssc_annotation(CardinalObj, ssc_data, resolution ="r=2,k=8,s=32")
+add_ssc_annotation <- function(data, data_ssc, resolution){
 
   data_bin <- data
 
@@ -25,10 +25,9 @@ add_ssc_annotation <- function(data, data_ssc, resolution = 25){
     if(class(data_ssc) != 'ResultsList'){
       Cardinal::pixelData(data_bin)[["ssc"]] <- data_ssc$class
     } else {
-      res <- names(data_bin)[grepl(names(data_ssc), pattern = paste0("s=", resolution))]
-      message(paste0("Getting cluster segments for resolution ", res))
+      message(paste0("Getting cluster segments for resolution ", resolution))
 
-      Cardinal::pixelData(data_bin)[["ssc"]] <- data_ssc@listData[[res]]$class
+      Cardinal::pixelData(data_bin)[["ssc"]] <- data_ssc@listData[[resolution]]$class
     }
 
   } else {
